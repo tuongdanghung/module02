e309f22ced828022c7d14b4709394bfdf3230dd9
tuongdanghung <tuongdanghung37@gmail.com>
2023-07-22 10:19:28 +0700

    first commit

---
+++ src/utils/path.js
@@ -0,0 +1,28 @@
+const path = {
+    PUBLIC: '/',
+    HOME: '',
+    ALL: '*',
+    LOGIN: 'login',
+    PRODUCTS: 'products',
+    PRODUCTS_DETAIL: 'products/:id',
+    BLOG: 'blog',
+    FAQ: 'faq',
+    PROFILE: 'profile',
+    OUR_SERVICES: 'OUR SERVICES',
+    FINALREGISTER: 'finalregister/:status',
+    FORGOT: 'reset-password/:token',
+    // Admin
+    ADMIN: 'admin',
+    DASHBOARD: 'dashboard',
+    MANAGER_USER: 'manage-user',
+    MANAGER_PRODUCT: 'manage-product',
+    MANAGER_ORDER: 'manage-oder',
+    CREATE_PRODUCT: 'create-product',
+    // Member
+    MEMBER: 'member',
+    PERSONAL: 'personal',
+}
+
+
+
+export default path
